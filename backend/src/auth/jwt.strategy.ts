0189febdb276b46b17d5db2b6069504e6736b0e8
Léo <leo.cigale@gmail.com>
2025-11-09 15:06:50 +0100

    FIX: include test in tsconfig.json

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -7,15 +7,16 @@ interface JwtPayload {
   sub: string;
   email: string;
 }
+interface RequestWithCookies extends Request {
+  cookies: Record<string, string>;
+}
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (req: Request) => {
-          return req?.cookies?.jwt;
-        },
+        (req: RequestWithCookies) => req?.cookies?.jwt ?? null,
       ]),
       secretOrKey: 'SECRET_KEY',
     });
