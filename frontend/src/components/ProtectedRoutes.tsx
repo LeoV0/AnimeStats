1eaa0526e8e159df3e14e4d9e298f48609f4bfa8
Léo <leo.cigale@gmail.com>
2025-11-19 11:36:30 +0100

    FIX: Background correct when loading for isLoggedIn

--- frontend/src/components/ProtectedRoutes.tsx
+++ frontend/src/components/ProtectedRoutes.tsx
@@ -5,7 +5,12 @@ import type { ReactNode } from "react";
 export default function ProtectedRoute({ children }: { children: ReactNode }) {
   const { isLoggedIn, loading } = useAuth();
 
-  if (loading) return <p>Chargement...</p>;
+  if (loading)
+    return (
+      <div className="w-full min-h-screen bg-gradient-to-br from-black via-[#0B0F14] to-[#1A2428] flex items-center justify-center text-white">
+        <p>Chargement...</p>
+      </div>
+    );
 
   if (!isLoggedIn) return <Navigate to="/login" replace />;
 
