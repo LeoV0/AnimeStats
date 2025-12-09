e372a55e2a04943246503e2efa94213604df6a60
Léo <leo.cigale@gmail.com>
2025-12-09 11:51:16 +0100

    FIX: import ConfigModule in AuthModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,14 +4,16 @@ import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule,
     JwtModule.register({
-      secret: 'SECRET_KEY',
-      signOptions: { expiresIn: '1h' },
+      secret: process.env.JWT_SECRET || 'fallback-secret',
+      signOptions: { expiresIn: '7d' },
     }),
+    ConfigModule.forRoot(),
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
