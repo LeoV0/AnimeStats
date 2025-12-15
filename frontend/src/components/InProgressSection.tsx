eec16fd4199180a7167eb2b1aa28929a0c5899cd
Léo <leo.cigale@gmail.com>
2025-12-15 11:00:11 +0100

    DELETE: Progress in AnimeCard

--- frontend/src/components/InProgressSection.tsx
+++ frontend/src/components/InProgressSection.tsx
@@ -95,7 +95,6 @@ export default function InProgressSection() {
                 </div>
               </Badge>
             }
-            progress={`Épisode ${anime.lastSeenEpisode}`}
           />
         ))}
       </div>
